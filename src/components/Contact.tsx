dc24865cb69a1b79ca5de014fc9c00b177c6eea6
Admin <iheachochristian@yahoo.com>
2025-12-17 11:53:38 +0000

    Redesigned the banner

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -54,7 +54,7 @@ const Contact = () => {
 
 
   return (
-    <section className="pt-20 pb-20" id="contact">
+    <section className="pt-20 pb-20 bg-gray-100" id="contact">
       <SectionTitle
         title="Get In Touch"
         subtitle="Ready to start your PTE journey? Contact us today"
