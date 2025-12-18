2657a3e09634cb24755eacb5eb7463b731b6fa90
Admin <iheachochristian@yahoo.com>
2025-12-18 16:49:09 +0000

    Added testimonial component and slider function

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,10 +2,9 @@ import SectionTitle from "./SectionnTitle";
 import TestimonialCard from "./TestimonialCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination  } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/pagination";
 import { testimonials } from "../util/testimonial";
+import "swiper/css"
+import "swiper/css/pagination";
 
 const Testimonial = () => {
   return (
